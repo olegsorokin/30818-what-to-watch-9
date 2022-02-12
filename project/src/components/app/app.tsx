b73767193dd3617ca276e9e319e7d54b3f823ad4
Oleg Sorokin <style-x55@mail.ru>
2022-02-12 13:11:14 +0600

    2.11. Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,20 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+
+export type Limit = number
+
+export type Film = {
+  title: string,
+  genre: string,
+  released: number
+}
+
+type AppProps = {
+  limit: Limit,
+  promoFilm: Film
+}
+
+function App({limit, promoFilm}: AppProps): JSX.Element {
+  return <Main limit={limit} promoFilm={promoFilm}/>;
 }
 
 export default App;
