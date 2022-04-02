48a8c24950543529645a245aad3cb9db8de8f9db
Oleg Sorokin <style-x55@mail.ru>
2022-04-02 17:55:42 +0600

    Create middleware

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from 'axios';
 const BACKEND_URL = 'https://9.react.pages.academy/wtw';
 const REQUEST_TIMEOUT = 5000;
 
-export function createApi(): AxiosInstance {
+export function createAPI(): AxiosInstance {
   return axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
