91eb191e94be33c70a1f666483dd6e43a11d3508
Oleg Sorokin <style-x55@mail.ru>
2022-04-10 14:14:26 +0600

    Clean the code

--- project/src/constants/review.ts
+++ project/src/constants/review.ts
@@ -5,4 +5,6 @@ export const MAX_COMMENT_LENGTH = 400;
 export const DEFAULT_RATING = 5;
 
 const STARS_COUNT = 10;
-export const STARS_ARRAY = new Array(STARS_COUNT).fill(0).map((_, index) => String(STARS_COUNT - index));
+export const STARS_ARRAY = new Array(STARS_COUNT)
+  .fill(0)
+  .map((_, index) => String(STARS_COUNT - index));
